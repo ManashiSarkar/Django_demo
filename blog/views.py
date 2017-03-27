0e166e060a8f6ddf7bfb98e5ee2abf8a1379636f
monti <manashi.ncr@gmail.com>
2017-03-27 22:33:32 +0530

    using template tags

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request,pk):
+	posts = Post.objects.filter(pk=pk)
+	return render(request, 'blog/post_detail.html', {'posts': posts})
