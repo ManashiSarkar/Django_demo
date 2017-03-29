ebfa82c46dfd6131ea0c22014f68db820733d381
monti <manashi.ncr@gmail.com>
2017-03-29 15:59:53 +0530

    accounts/signup working properly

--- userinfo/forms.py
+++ userinfo/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 
 class Signup(forms.Form):
-	First_name = forms.CharField(label='first_name',required=True,max_length=20)
-	Last_name = forms.CharField(label='last_name',required=True,max_length=20)
+	First_name = forms.CharField(label='First name',required=True,max_length=20)
+	Last_name = forms.CharField(label='Last name',required=True,max_length=20)
 	
 	def signup(self, request, user):
-		user.First_name = self.First_name
-		user.Last_name = self.Last_name
+		user.first_name = self.cleaned_data['First_name']
+		user.last_name = self.cleaned_data['Last_name']
+		user.save()
